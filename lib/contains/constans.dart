7ec7225743fcdb343ac3825d5fe4de4d4137a094
Ngapa <krilinamar@gmail.com>
2022-07-05 01:36:51 +0700

    Linting

--- lib/contains/constans.dart
+++ lib/contains/constans.dart
@@ -4,8 +4,8 @@ const Color primaryColor = Color(0xff0A0E21);
 TextStyle? labelTextStyle =
     const TextStyle(fontSize: 18, color: Colors.white //Color(0xff8d8e98),
         );
-TextStyle numberTextStyle =
-    TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white);
+TextStyle numberTextStyle = const TextStyle(
+    fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white);
 
 const String underweightSevere = "Underweight (Severe thinness)";
 const String underweightModerate = "Underweight (Moderate thinnes)";
